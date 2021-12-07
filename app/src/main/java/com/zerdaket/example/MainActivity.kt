032429cb9283ee34fe2b87d2866302b858ea5272
zerdaket <zerdaket@gmail.com>
2021-12-07 22:35:29 +0800

    新增展示app信息列表

--- app/src/main/java/com/zerdaket/example/MainActivity.kt
+++ app/src/main/java/com/zerdaket/example/MainActivity.kt
@@ -2,10 +2,28 @@ package com.zerdaket.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zerdaket.example.adapter.AppAdapter
+import com.zerdaket.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val adapter = AppAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.recycler.layoutManager = LinearLayoutManager(this)
+        binding.recycler.adapter = adapter
+
+        viewModel.appListLiveData.observe(this, {
+            adapter.setAppList(it)
+        })
+
+        viewModel.getAppList()
     }
+
 }
---
+++ app/src/main/java/com/zerdaket/example/adapter/AppAdapter.kt
@@ -0,0 +1,42 @@
+package com.zerdaket.example.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.zerdaket.example.R
+import com.zerdaket.example.data.AppInfo
+
+/**
+ * @author zerdaket
+ * @date 2021/12/7 10:06 下午
+ */
+class AppAdapter: RecyclerView.Adapter<AppAdapter.AppViewHolder>() {
+
+    private var appList: List<AppInfo>? = null
+
+    fun setAppList(list: List<AppInfo>) {
+        appList = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_icon, parent, false)
+        return AppViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AppViewHolder, position: Int) {
+        val info = appList?.get(holder.bindingAdapterPosition) ?: return
+        holder.imageView.setImageDrawable(info.icon)
+        holder.textView.text = info.name
+    }
+
+    override fun getItemCount(): Int = appList?.size ?: 0
+
+    class AppViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.name)
+        val imageView: ImageView = itemView.findViewById(R.id.image)
+    }
+}
